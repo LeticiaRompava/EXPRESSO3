9969f895acb156ed6b9f1c673cb4657b069c76b6
LeticiaRompava <leticia.rompava@escola.pr.gov.br>
2024-08-12 10:42:30 -0300

    subir

---
+++ server.js
@@ -0,0 +1,27 @@
+//criar uma query(consulta) simples
+const express = require('express')
+const mysql = require('mysql2')
+
+const app = express();
+
+app.listen(3000, ()=>{
+    console.log("Servidor em execução!");
+})
+
+//criação da conexão com o banco de dados
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user_bd_tasks',
+    password: 'QLOP4TDcQGB2R97Djet&vXYHggatTSZE4',
+    database: 'nodejs_tasks'
+})
+
+//testar a conexão
+connection.connect(error =>{
+    if(error){
+        console.log('Erro na conexão ao MySql:'+ error.message)
+        return
+    }
+    console.log('Conexão realizada com sucesso!')
+})
+
